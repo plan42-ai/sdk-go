3e9440b7abcf0690b79ad9fd6fbc612cb20d556b
Plan42.ai <robot@plan42.ai>
2025-10-08 22:08:06 +0000

    eh-ctl: add workstream get command

--- cmd/eh-ctl/workstream.go
+++ cmd/eh-ctl/workstream.go
@@ -12,6 +12,7 @@ import (
 // WorkstreamOptions is the root for all workstream related sub-commands.
 type WorkstreamOptions struct {
 	Create CreateWorkstreamOptions `cmd:""`
+	Get    GetWorkstreamOptions    `cmd:""`
 }
 
 // CreateWorkstreamOptions contains the flags for the `workstream create` command.
@@ -54,6 +55,34 @@ func (o *CreateWorkstreamOptions) Run(ctx context.Context, s *SharedOptions) err
 	processDelegatedAuth(s, &req.DelegatedAuthInfo)
 
 	ws, err := s.Client.CreateWorkstream(ctx, &req)
+	if err != nil {
+		return err
+	}
+
+	return printJSON(ws)
+}
+
+// GetWorkstreamOptions contains the flags for the `workstream get` command.
+type GetWorkstreamOptions struct {
+	TenantID       string `help:"The id of the tenant to get the workstream from." name:"tenant-id" short:"i" required:""`
+	WorkstreamID   string `help:"The id of the workstream to get." name:"workstream-id" short:"w" required:""`
+	IncludeDeleted bool   `help:"Include deleted workstreams in the result." short:"d" optional:""`
+}
+
+func (o *GetWorkstreamOptions) Run(ctx context.Context, s *SharedOptions) error {
+	req := &eh.GetWorkstreamRequest{
+		TenantID:       o.TenantID,
+		WorkstreamID:   o.WorkstreamID,
+		IncludeDeleted: pointer(o.IncludeDeleted),
+	}
+
+	if err := loadFeatureFlags(s, &req.FeatureFlags); err != nil {
+		return err
+	}
+
+	processDelegatedAuth(s, &req.DelegatedAuthInfo)
+
+	ws, err := s.Client.GetWorkstream(ctx, req)
 	if err != nil {
 		return err
 	}
